effc6a5d26d33ad7c68504ab299506c5ace5f170
Dylan Dinio <DDinio2101@gmail.com>
2023-04-13 17:54:02 -0400

    Fixed searching bugs in

--- vet-finder-app/src/pages/Search.js
+++ vet-finder-app/src/pages/Search.js
@@ -7,18 +7,19 @@ const Search = () =>{
 
     const filter_clinics = (e)=>{
       const query = e.target.value.toLowerCase();
-      const filtered_clinics = clinicData.slice().filter((clinic) => clinic.clinic_name.toLowerCase().includes(query))
-  
+      // console.log("Clinics being read: " + clinics);
+      const filtered_clinics = clinicData.filter((clinic) => clinic.clinic_name.toLowerCase().includes(query));
       setClinics(filtered_clinics);
+      const names_of_clinics = clinics.map((c) => c.clinic_name);
     }
   
     return (
       <div className="App">
         <label>Search</label>
-        <input type="text" onChange={(e) => filter_clinics(e)}/>
+        <input type="text" onChange={(e) => {filter_clinics(e)}}/>
           <ul>
             {clinics.map((data) => {
-              return <h1 key = {data.clinic_name}> {data.clinic_name}</h1>
+              return <h1 key = {data.clinic_name + data.zip + data.specialities[0]}> {data.clinic_name}</h1>
             })}
           </ul>
       </div>

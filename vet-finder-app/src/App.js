6d2d41b640e4f77d9215ac4791457411812bae16
Geneva Yang <yyang25@bu.edu>
2023-04-13 17:03:35 -0400

    welcome page need to debug

--- vet-finder-app/src/App.js
+++ vet-finder-app/src/App.js
@@ -1,29 +1,18 @@
 import './App.css';
 import { useState } from "react";
-import { clinicData } from './clinic-info';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Welcome from './script/welcome';
+
 
 
 function App() {
-  const [clinics, setClinics] = useState(clinicData);
-
-  const filter_clinics = (e)=>{
-    const query = e.target.value.toLowerCase();
-    const filtered_clinics = clinicData.slice().filter((clinic) => clinic.clinic_name.toLowerCase().includes(query))
-
-    setClinics(filtered_clinics);
-    
-  }
 
   return (
-    <div className="App">
-      <label>Search</label>
-      <input type="text" onChange={(e) => filter_clinics(e)}/>
-        <ul>
-          {clinics.map((data) => {
-            return <h1 key = {data.clinic_name}> {data.clinic_name}</h1>
-          })}
-        </ul>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element = {<Welcome />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

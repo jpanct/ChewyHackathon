ad2458b6b571cf99922fe1ee90179229d2d0583e
Dylan Dinio <DDinio2101@gmail.com>
2023-04-13 13:50:06 -0400

    fixed the first page of the website

--- vet-finder-app/src/App.js
+++ vet-finder-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const filter_clinics = (e)=>{
     const query = e.target.value.toLowerCase();
-    const filtered_clinics = clinicData.filter((clinic) => clinic.clinic_name.toLowerCase().includes(query))
+    const filtered_clinics = clinicData.slice().filter((clinic) => clinic.clinic_name.toLowerCase().includes(query))
 
     setClinics(filtered_clinics);
     
@@ -18,11 +18,11 @@ function App() {
     <div className="App">
       <label>Search</label>
       <input type="text" onChange={(e) => filter_clinics(e)}/>
-      <ul>
-        {clinics.map((data) => {
-          return <h1 key = {data.clinic_name}> {data.clinic_name}</h1>
-        })}
-      </ul>
+        <ul>
+          {clinics.map((data) => {
+            return <h1 key = {data.clinic_name}> {data.clinic_name}</h1>
+          })}
+        </ul>
     </div>
   );
 }
